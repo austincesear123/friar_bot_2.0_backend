121b05ef1b35c3077a356f4b313a8f2a9c0c39cb
Austin Cesear <austincesear123@gmail.com>
2022-05-09 11:43:07 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.post("/friar_bot/upload", (req, res) => {
+  res.send("upload image route hit");
+});
+
+app.listen(3000, () => console.log("listening on port 3000"));
