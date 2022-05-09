96e16f825361c6fbd414b7a6c076cdf4a7a18726
Austin Cesear <austincesear123@gmail.com>
2022-05-09 12:11:23 -0700

    setup for s3

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+require("dotenv").config();
+import AWS from "aws-sdk";
 
 const app = express();
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+});
 
 app.post("/friar_bot/upload", (req, res) => {
   res.send("upload image route hit");
